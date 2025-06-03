12694de47628bd3d107886e28b870f7dfc9c7d2a
tupupindo <hackerindon999@gmail.com>
2025-06-03 15:54:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("TopUpKuy aktif!");
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".game-card");
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+            const gameId = card.dataset.gameId;
+            if (gameId) {
+                window.location.href = `game-detail.html?game=${gameId}`;
+            }
+        });
+    });
+});
